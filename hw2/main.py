971a3972e4e9dbdc3a5f9a79901f653c84518960
Michael Zhang <mzhang2016@gmail.com>
2019-02-21 14:09:33 -0500

    Remove debugging bug

--- hw2/main.py
+++ hw2/main.py
@@ -19,12 +19,14 @@ from namedtensor.text import NamedField
 from nnlm import NNLM  
 from lstm import RNN 
 
+
 def repackage_hidden(h):
     """Detach hidden state tensors from history w/ new batch"""
     if isinstance(h, torch.Tensor):
         return h.detach()
     else:
         return tuple(repackage_hidden(v) for v in h) 
+
 
 def train(model, train_iter, criterion, optimizer, lr=0.0001, weight_decay=0): 
     """Method to train model. Define optimizer = ADAM, Adagrad, etc (e.g. optim.Adam); criterion = torch.nn.NLLLoss()""" 
@@ -33,14 +35,26 @@ def train(model, train_iter, criterion, optimizer, lr=0.0001, weight_decay=0):
     model.train()
     optimizer = optimizer(model.parameters(), lr=lr, weight_decay=weight_decay)
 
+    if model.rnn:
+        hidden = model.init_hidden(BATCH_SIZE)
+
     for ix in range(len(x_train)):
         optimizer.zero_grad()
         x_batch = x_train[ix]; y_batch = y_train[ix]
 
-        probs = model.forward(x_batch)
-        loss = criterion(props.log(), y_batch)
-
-        loss.backward()
+        if model.rnn:
+            hidden = repackage_hidden(hidden)
+            output, hidden = model.forward(x_batch, hidden)
+            loss = criterion(output.view(-1, VOCAB_SIZE).log(), y_batch)
+            loss.backward()
+            # Helps prevent the exploding gradient problem in RNNs / LSTMs
+            torch.nn.utils.clip_grad_norm(model.paramters(), 0.25)  
+            for p in model.parameters():
+                p.data.add_(-lr, )
+        else:
+            probs = model.forward(x_batch)
+            loss = criterion(probs.log(), y_batch)
+            loss.backward()
         optimizer.step()
 
     return validate(model, x_train, y_train, n_train)
@@ -50,13 +64,21 @@ def validate(model, x, y, n_words):
     """Method to evaluate model"""
     model.eval()
 
+    if model.hidden:
+        hidden = model.init_hidden(BATCH_SIZE)
+
     with torch.no_grad():
         criterion = torch.nn.NLLLoss(reduction='sum')
         epoch_loss = 0
         n_correct = 0
 
         for i in range(len(x)):
-            probs = model.forward(x[i])
+            if model.rnn:
+                probs, hidden = model.forward(x[i], hidden)
+                probs = probs.view(-1, VOCAB_SIZE)
+                hidden = repackage_hidden(hidden)
+            else:
+                probs = model.forward(x[i])
             epoch_loss += criterion(probs.log(), y[i])
             n_correct += (probs.argmax(dim=1) == y[i]).sum().float()
         loss = epoch_loss / n_words
@@ -72,6 +94,10 @@ def train_val_model(model, n_epochs, criterion, optimizer):
         print(f'| Epoch: {epoch+1:02} | Train Acc: {train_acc*100:.2f}% | \
             Train Loss: {train_loss:.3f} | Train Ppl: {train_ppl:.3f} | \
             Val. Acc: {val_acc*100:.2f}% | Val. Loss: {val_loss:.3f} | Val. Ppl: {val_ppl:.3f}')
+
+
+BATCH_SIZE = 10  
+VOCAB_SIZE = len(TEXT.vocab)
 
 
 
