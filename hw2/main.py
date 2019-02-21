2cf98285a1bce359931d1796c494ce1417261e4b
Michael Zhang <mzhang2016@gmail.com>
2019-02-21 14:28:32 -0500

    Fix more notational issues

--- hw2/main.py
+++ hw2/main.py
@@ -28,7 +28,7 @@ def repackage_hidden(h):
         return tuple(repackage_hidden(v) for v in h) 
 
 
-def train(model, train_iter, criterion, optimizer, lr=0.0001, weight_decay=0): 
+def train_model(model, train_iter, criterion, optimizer, lr=0.0001, weight_decay=0): 
     """Method to train model. Define optimizer = ADAM, Adagrad, etc (e.g. optim.Adam); criterion = torch.nn.NLLLoss()""" 
     x_train, y_train, n_train = model.get_data(train_iter)
 
@@ -57,10 +57,10 @@ def train(model, train_iter, criterion, optimizer, lr=0.0001, weight_decay=0):
             loss.backward()
         optimizer.step()
 
-    return validate(model, x_train, y_train, n_train)
+    return validate_model(model, x_train, y_train, n_train)
 
 
-def validate(model, x, y, n_words):
+def validate_model(model, x, y, n_words):
     """Method to evaluate model"""
     model.eval()
 
@@ -88,9 +88,9 @@ def validate(model, x, y, n_words):
 def train_val_model(model, n_epochs, criterion, optimizer):  
     """Wrapper to call training and evaluation"""  
     for epoch in range(n_epochs):
-        train_acc, train_loss, train_ppl = train(model, train_iter, criterion, optimizer, 0.0001, 0)
+        train_acc, train_loss, train_ppl = train_model(model, train_iter, criterion, optimizer, 0.0001, 0)
         x_val, y_val, n_val = model.get_data(val_iter)
-        val_acc, val_loss, val_ppl = validate(model, x_val, y_val, n_val)
+        val_acc, val_loss, val_ppl = validate_model(model, x_val, y_val, n_val)
         print(f'| Epoch: {epoch+1:02} | Train Acc: {train_acc*100:.2f}% | \
             Train Loss: {train_loss:.3f} | Train Ppl: {train_ppl:.3f} | \
             Val. Acc: {val_acc*100:.2f}% | Val. Loss: {val_loss:.3f} | Val. Ppl: {val_ppl:.3f}')
