a8452950a470ee1bb42230a10dadec586df59278
Michael Zhang <mzhang2016@gmail.com>
2019-02-22 17:05:56 -0500

    Implement working neural language model

--- hw2/nnlm.py
+++ hw2/nnlm.py
@@ -1,79 +1,36 @@
 from data_prep import torch, TEXT  
 
-WORD_VECS = TEXT.vocab.vectors  
-
-BATCH_SIZE = 10
-
-class NNLM(torch.nn.Module):
-    """Implement neural net convolutional language model inspired by Bengio et al. 2003"""  
-    def __init__(self, n, hidden_size=100, num_layers=3, dropout_rate=0.3, num_filters=50):
-        super().__init__()
-      
-        self.n = n
-        self.vocab_size = len(TEXT.vocab)
-        self.dropout = torch.nn.Dropout(p=dropout_rate)
-        self.embeddings = torch.nn.Embedding.from_pretrained(WORD_VECS.clone(), freeze=False)
-        self.conv = torch.nn.Conv1d(300, num_filters, n-1, stride=1)
-
-        self.recurrent = False
+class NNLM(nn.Module):
+    """Language Model inspired by Bengio et al. 2003"""
+    def __init__(self, hidden_dim=100, TEXT = TEXT):
+        super(NNLM, self).__init__()
         
-        # Setup layers  
-        linear_blocks = [  # input
-            torch.nn.Linear(num_filters, hidden_size),
-            torch.nn.Tanh(),]
-        for l in range(num_layers - 2):  # hidden
-            linear_blocks.append(torch.nn.Linear(num_filters, hidden_size))
-            linear_blocks.append(torch.nn.Tanh())
-        linear_blocks.extend([  # output
-            torch.nn.Linear(hidden_size, self.vocab_size), 
-            torch.nn.Softmax(dim=-1)])
+        vocab_size, embedding_dim = TEXT.vocab.vectors.shape
         
-        self.out = torch.nn.Sequential(*linear_blocks)
+        self.embeddings = nn.Embedding(vocab_size, embedding_dim)
+        self.embeddings.weight.data.copy_(TEXT.vocab.vectors)
+        
+        self.input = nn.Linear(2*embedding_dim, hidden_dim)
+        self.output = nn.Linear(hidden_dim, vocab_size)
+        self.TEXT = TEXT
         
     def forward(self, x):
-        x = x.transpose(0, 1)
-        embeddings = self.dropout(self.embeddings(x)).permute(1, 2, 0)
-        convs = self.conv(embeddings).transpose(1, 2)
-        
-        out = self.out(convs).flatten(0, 1)
-        return out
-      
-    def format_batch(self, batch):
-        list_samples = []
-        list_results = []
-        
-        num_words = 0
-        
-        for i in range(len(batch)):
-            context = batch.text[{'batch': i}].values.data
-            targets = batch.target.get('batch', i).values.data  
-            
-            num_words += len(context)
-            padded_context = torch.nn.functional.pad(context, (self.n - 2, BATCH_SIZE - len(context)), value=0)
-            target_words = torch.nn.functional.pad(targets, (0, BATCH_SIZE - len(targets)), value=0)
-            
-            list_samples.append(padded_context)
-            list_results.append(target_words)
-            
-        samples_all = torch.stack(list_samples)
-        results_all = torch.cat(list_results)
-        
-        return (samples_all, results_all, num_words)
+        x = self.embeddings(x)
+        x = x.view(len(x), -1)
+        x = F.tanh(self.input(x))
+        x = self.output(x)
+        return F.log_softmax(x, dim=1)
       
       
-    def get_data(self, batch_iter):
-        list_batch_x = []
-        list_batch_y = []
-        
-        total_words = 0
-        
-        for batch in batch_iter:
-            x, y, num_words = self.format_batch(batch)
-            list_batch_x.append(x)
-            list_batch_y.append(y)
-            total_words += num_words
-
-        return list_batch_x, list_batch_y, total_words
+    def predict(self, x):
+        """Predict things"""
+        embed = self.embeddings(x)
+        embed = embedded.view(-1, 1).transpose(0, 1)
+        activated = nn.Tanh(self.input(embed))
+        logits = F.log_softmax(output, dim = 1)
+        out_ids = np.argsort(logits.data[0].tolist())[-20:][::-1]
+        out_words = ' '.join([TEXT.vocab.itos[out_id] for out_id in out_ids])
+        return out_words
             
         
             
