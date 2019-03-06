96b551bf5abbee65e32cdd791d9a7a5c7f504443
Roshan Padaki <roshanpadaki@gmail.com>
2019-03-06 10:46:00 -0500

    bugfix

--- hw3/models.py
+++ hw3/models.py
@@ -34,7 +34,7 @@ class DecoderLSTM(EncoderLSTM):
     """Inherit same architecture as encoder, but reversed"""
     def __init__(text, hidden_size=500, num_layers=2, dropout=0.0,
                  bidirectional_encoder=False, context_size=1, **kwargs):
-        super(DeocderLSTM, self).__init__(text, **kwargs)
+        super(DecoderLSTM, self).__init__(text, **kwargs)
         self.encoder_directions = 2 if bidirectional_encoder else 1
         self.context_size = context_size
         
